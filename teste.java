427244093cfcb0633c65254bbbd9006d1f623880
Andrieli Paula Frana <andrielifrana@gmail.com>
2025-03-14 20:20:42 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -16,3 +16,31 @@ class Aluno {
     public int getIdade() {
         return idade;
     }
+
+    class Turma {
+    private String codigo;
+    private String etapaEnsino;
+    private int ano;
+    private int limiteVagas;
+    private List<Aluno> alunos;
+    
+    public Turma(String codigo, String etapaEnsino, int ano, int limiteVagas) {
+        this.codigo = codigo;
+        this.etapaEnsino = etapaEnsino;
+        this.ano = ano;
+        this.limiteVagas = limiteVagas;
+        this.alunos = new ArrayList<>();
+    }
+     @Override
+    public String toString() {
+        return "Nome: " + nome + ", CPF: " + cpf + ", Endereço: " + endereco + ", Idade: " + idade;
+    }
+}
+
+    public boolean adicionarAluno(Aluno aluno) {
+        if (alunos.size() < limiteVagas && validarIdade(aluno.getIdade())) {
+            alunos.add(aluno);
+            return true;
+        }
+        return false;
+    }
